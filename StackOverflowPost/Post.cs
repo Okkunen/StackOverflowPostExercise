a5840e84d088888f31e73dae4906276dd5ae9d16
Okkunen <tsemi@hotmail.com>
2020-01-01 20:35:28 +0200

    Add project files.

---
+++ StackOverflowPost/Post.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace StackOverflowPost
+{
+    public class Post
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int Score { get; private set; } = 0;
+
+        public Post(string title, string description)
+        {
+            Title = title;
+            Description = description;
+            CreatedDate = DateTime.UtcNow;
+        }
+
+        public void Upvote()
+        {
+            Score++;
+        }
+
+        public void Downvote()
+        {
+            Score--;
+        }
+        public override string ToString()
+        {
+            string output = String.Format("Post title: {0}, post description: {1}, Current vote: {2}, post was created on {3}.", Title, Description, Score, CreatedDate);
+            return output;
+        }
+    }
+}
---
+++ StackOverflowPost/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackOverflowPost
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Code for testing
+            Post postTest = new Post("Test post", "Test description for post");
+            postTest.Upvote();
+            postTest.Upvote();
+            Console.WriteLine(postTest);
+            postTest.Downvote();
+            Console.WriteLine(postTest);
+            postTest.Downvote();
+            postTest.Downvote();
+            Console.WriteLine(postTest);
+            Console.ReadLine();
+
+        }
+    }
+}
